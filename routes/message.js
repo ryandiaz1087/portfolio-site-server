880e7d244cbdbb5348f1cb566d406bc7581d5ad4
Ryan Diaz <ryandiaz@Ryans-MacBook-Pro.local>
2020-07-07 10:56:27 -0700

    create message route created and headers added

--- routes/message.js
+++ routes/message.js
@@ -4,10 +4,10 @@ const { getSingleMessage, createMessage, getMessages } = require('../controllers
 const router = express.Router();
 
 router.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*"); 
-  res.header("Access-Control-Allow-Headers", "Origin, Authorization, X-Requested-With, Content-Type, Accept, X-Access-Token, x-auth-token");
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
   next();
 });
 
